f8092ec7ae69ec6eac0fadc23be0f577c9c45247
Manish-061 <kumarmanish092003@gmail.com>
2026-04-23 10:56:12 +0530

    Initial Commit

---
+++ smart-expense-backend/src/main/java/com/smartexpense/smartexpensebackend/SmartExpenseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartexpense.smartexpensebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExpenseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartExpenseBackendApplication.class, args);
+	}
+
+}
---
+++ smart-expense-backend/src/test/java/com/smartexpense/smartexpensebackend/SmartExpenseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartexpense.smartexpensebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartExpenseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
