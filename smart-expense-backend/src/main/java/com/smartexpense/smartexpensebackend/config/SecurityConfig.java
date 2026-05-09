103f7bd65221a5ebccf4cced349d189e8bb39686
Manish-061 <kumarmanish092003@gmail.com>
2026-05-09 12:52:42 +0530

    updated allowedorigins to handle cors error

--- smart-expense-backend/src/main/java/com/smartexpense/smartexpensebackend/config/SecurityConfig.java
+++ smart-expense-backend/src/main/java/com/smartexpense/smartexpensebackend/config/SecurityConfig.java
@@ -44,7 +44,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000", 
+                "http://localhost:5173",
+                "https://smart-expense-beta.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
